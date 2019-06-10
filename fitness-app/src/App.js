45e18316ce9b77148b68d5b6e489bcbc33d3c4ea
Josh Akeman <joshakeman@gmail.com>
2019-06-10 17:10:48 -0400

    created react app, added header/footer

---
+++ fitness-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+      <Header />
+      <Footer />
+      </div>
+      );
+  } 
+
+}
+
+export default App;
